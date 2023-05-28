ed47b8e8216816458567a30d525e3b405c93ca16
Asad-Ismail <asadismaeel@gmail.com>
2023-05-28 20:01:16 +0000

    added simple onnx model

--- onnx_conversion.py
+++ onnx_conversion.py
@@ -5,6 +5,11 @@ import onnxruntime
 from nanodet.data.transform import Pipeline
 from nanodet.model.arch import build_model
 from nanodet.util import Logger, cfg, load_config, load_model_weight
+import onnxsim
+import onnx
+import warnings
+
+warnings.filterwarnings('ignore') # Ignore warnings
 
 # Configurations
 cfg_path = "config/nanoinstance-mask-512.yml"
@@ -96,6 +101,55 @@ def main():
         print('Difference in scores: ', np.abs(torch_scores.detach().numpy() - onnx_scores).max())
     
     print(f"All tests succedded the conversion to ONNX was successful!!")
+    
+    print("start simplifying onnx ")
+    input_data = {"images": img_tensor.detach().cpu().numpy()}
+    model_sim, flag = onnxsim.simplify(out_path, input_data=input_data)
+    if flag:
+        onnx.save(model_sim, out_path)
+        print("simplify onnx successfully")
+    else:
+        print("simplify onnx failed")
+
+
+    # Load Simplified ONNX model
+    print(f"Loading Simplified ONNX model!!")
+    ort_session = onnxruntime.InferenceSession("segmentor.onnx")
+    print(f"Loaded Simplified ONNX model!!")
+
+    # Run inference on ONNX model
+    ort_inputs = {ort_session.get_inputs()[0].name: img_tensor.numpy()}
+    ort_outs = ort_session.run(None, ort_inputs)
+
+    onnx_bbox, onnx_msks, onnx_labels, onnx_scores = ort_outs
+
+    # Assert that all outputs have equal shape
+    assert torch_bbox.shape == onnx_bbox.shape
+    assert torch_msks.shape == onnx_msks.shape
+    assert torch_labels.shape == onnx_labels.shape
+    assert torch_scores.shape == onnx_scores.shape
+
+    # Assert that all outputs are close
+    assert np.allclose(torch_bbox.detach().numpy(), onnx_bbox, rtol=1e-03, atol=1e-05)
+    assert np.allclose(torch_msks.detach().numpy(), onnx_msks, rtol=1e-03, atol=1e-05)
+    assert np.allclose(torch_labels.detach().numpy(), onnx_labels, rtol=1e-03, atol=1e-05)
+    assert np.allclose(torch_scores.detach().numpy(), onnx_scores, rtol=1e-03, atol=1e-05)
+
+    # Print differences if shapes are not equal or outputs are not close
+    if not np.allclose(torch_bbox.detach().numpy(), onnx_bbox, rtol=1e-03, atol=1e-05):
+        print('Difference in bbox: ', np.abs(torch_bbox.detach().numpy() - onnx_bbox).max())
+
+    if not np.allclose(torch_msks.detach().numpy(), onnx_msks, rtol=1e-03, atol=1e-05):
+        print('Difference in masks: ', np.abs(torch_msks.detach().numpy() - onnx_msks).max())
+
+    if not np.allclose(torch_labels.detach().numpy(), onnx_labels, rtol=1e-03, atol=1e-05):
+        print('Difference in labels: ', np.abs(torch_labels.detach().numpy() - onnx_labels).max())
+
+    if not np.allclose(torch_scores.detach().numpy(), onnx_scores, rtol=1e-03, atol=1e-05):
+        print('Difference in scores: ', np.abs(torch_scores.detach().numpy() - onnx_scores).max())
+    
+    print(f"All tests succedded the conversion to ONNX Simplify was successful!!")
+
 
 if __name__ == "__main__":
     main()
