76a2c2330741efbad1e1d25aa058f14ed1553ab0
Asad-Ismail <asadismaeel@gmail.com>
2023-05-30 21:10:30 +0000

    updated inference

--- onnx_inference.py
+++ onnx_inference.py
@@ -5,6 +5,11 @@ import onnxruntime
 from nanodet.data.transform import PipelineInference
 from nanodet.model.arch import build_model
 from nanodet.util import Logger, cfg, load_config, load_model_weight
+from nanodet.data.transform.color import _normalize
 import onnxsim
 import onnx
 import warnings
+
+image_path = "data/cucumbers/113.png"
+img = cv2.imread(image_path)
+img=_normalize(img, mean=[103.53, 116.28, 123.675], std=[57.375, 57.12, 58.395])
