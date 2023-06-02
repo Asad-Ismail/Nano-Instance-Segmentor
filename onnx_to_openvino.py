c45eba186cd507bd90a3427822fe152026ce48b4
Asad-Ismail <asadismaeel@gmail.com>
2023-06-02 18:04:10 +0000

    updated conversion to openvino

--- onnx_to_openvino.py
+++ onnx_to_openvino.py
@@ -1,6 +1,21 @@
 # Construct the command for Model Optimizer.
 from pathlib import Path
 import subprocess
+import cv2
+import numpy as np
+import warnings
+from nanodet.data.transform.color import _normalize
+
+
+def load_image(image_path):
+    img = cv2.imread(image_path)
+    img = img.astype(np.float32) / 255
+    img = _normalize(img, mean=[103.53, 116.28, 123.675], std=[57.375, 57.12, 58.395])
+    img = img.transpose(2, 0, 1)
+    img = img[np.newaxis,...]
+    return img
+
+
 
 
 
@@ -13,7 +28,6 @@ IMAGE_WIDTH=512
 mo_command = f"""mo
                  --input_model "{onnx_path}"
                  --input_shape "[1,3, {IMAGE_HEIGHT}, {IMAGE_WIDTH}]"
-                 --data_type FP32
                  --output_dir "{model_path}"
                  """
 mo_command = " ".join(mo_command.split())
@@ -25,7 +39,7 @@ mo_result = subprocess.run(mo_command, shell=True, capture_output=True, text=Tru
 print(mo_result.stdout)
 
 
-ir_path=Path("irmodel/tinynet.xml")
+ir_path=Path("irmodel/segmentor.xml")
 from openvino.runtime import Core
 ie = Core()
 model_ir = ie.read_model(model=ir_path)
@@ -37,13 +51,15 @@ output_layer_ir1 = compiled_model_ir.output(1)
 output_layer_ir2 = compiled_model_ir.output(2)
 output_layer_ir3 = compiled_model_ir.output(3)
 
-res = compiled_model_ir([input_image])
+
+image_path = "data/cucumbers/113.png"
+img = load_image(image_path)
+res = compiled_model_ir([img])
+
+boxes=res[output_layer_ir0]
+masks=res[output_layer_ir1]
+labels=res[output_layer_ir2]
+scores=res[output_layer_ir3]
 
 
-res_ir0 = compiled_model_ir([input_image])[output_layer_ir0]
-res_ir1 = compiled_model_ir([input_image])[output_layer_ir1]
-res_ir2 = compiled_model_ir([input_image])[output_layer_ir2]
-res_ir3 = compiled_model_ir([input_image])[output_layer_ir3]
-
-
-print(res_ir0.shape)
+print(boxes.shape,masks.shape,labels.shape,scores.shape)
