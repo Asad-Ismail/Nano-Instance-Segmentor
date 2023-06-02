c1614354d5a5c55d52ab1336bd08a743616bb7df
Asad-Ismail <asadismaeel@gmail.com>
2023-06-02 17:43:39 +0000

    updated onnx to openvino

--- onnx_to_openvino.py
+++ onnx_to_openvino.py
@@ -1,6 +1,6 @@
 # Construct the command for Model Optimizer.
 from pathlib import Path
-onnx_path="tinynet.onnx"
+onnx_path="segmentor.onnx"
 model_path="irmodel"
 model_path = Path(model_path)
 ir_path = model_path.with_suffix(".xml")
@@ -19,3 +19,21 @@ print(mo_command)
 print("Exporting ONNX model to IR... This may take a few minutes.")
 mo_result = subprocess.run(mo_command, shell=True, capture_output=True, text=True)
 print(mo_result.stdout)
+
+
+# Get input and output layers.
+output_layer_ir0 = compiled_model_ir.output(0)
+output_layer_ir1 = compiled_model_ir.output(1)
+output_layer_ir2 = compiled_model_ir.output(2)
+output_layer_ir3 = compiled_model_ir.output(3)
+
+res = compiled_model_ir([input_image])
+
+
+res_ir0 = compiled_model_ir([input_image])[output_layer_ir0]
+res_ir1 = compiled_model_ir([input_image])[output_layer_ir1]
+res_ir2 = compiled_model_ir([input_image])[output_layer_ir2]
+res_ir3 = compiled_model_ir([input_image])[output_layer_ir3]
+
+
+print(res_ir0.shape)
